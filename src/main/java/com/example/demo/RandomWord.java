92ddc3e291e4cf3a129112018c635c028c1311a5
Ralph Bacolod1 <rafiks@gamma.devsphoto.com>
2022-01-09 11:04:06 -0500

    add template repo

---
+++ src/main/java/com/example/demo/RandomWord.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import edu.princeton.cs.introcs.StdIn;
+import edu.princeton.cs.introcs.StdOut;
+import edu.princeton.cs.introcs.StdRandom;
+
+public class RandomWord {
+  public static void main(String[] args) {
+    // read standard input
+    // return words randomly
+
+    // testing argument
+    //
+
+    System.out.println("args-length: " + args.length);
+
+    // if StdIn.isEmpty(){
+    // }
+    // StdIn.readString();
+    // StdIn.isEmpty();
+    // StdOut.println();
+    // StdRandom.bernoulli();
+  }
+}
