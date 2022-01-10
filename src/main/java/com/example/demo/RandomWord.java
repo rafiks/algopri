80714b43a7748021d8fe7e7b99f658009b2e046c
Ralph Bacolod1 <rafiks@gamma.devsphoto.com>
2022-01-10 17:10:47 -0500

    baseline working printout with random algorithm

--- src/main/java/com/example/demo/RandomWord.java
+++ src/main/java/com/example/demo/RandomWord.java
@@ -1,39 +1,14 @@
 package com.example.demo;
 
 import edu.princeton.cs.algs4.StdIn;
-
-// import edu.princeton.cs.introcs.StdIn;
-// import edu.princeton.cs.introcs.StdOut;
-// import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.algs4.StdRandom;
 
 public class RandomWord {
-  public static void main(String[] args) {
-    // read standard input
-    // return words randomly
-
-    // testing argument
-    //
-
-    String alll = StdIn.readString();
-    System.out.println(alll);
-    System.out.println("testing");
-    // String test = StdIn.readString();
-    // System.out.println("test");
-    // StdIn.readString()
-    // System.out.println("args-length: " + args.length); //
-
-    // if StdIn.isEmpty(){
-    // }
-    // StdIn.readString();
-    // StdIn.isEmpty();
-    // StdOut.println();
-    // StdRandom.bernoulli();
-  }
-
-  /**
-   * InnerRandomWord
-   */
-  public class randomWord {
+  public static void main(final String[] args) {
+    while (!StdIn.isEmpty()) {
+      String curword = StdIn.readString();
+      System.out.println(curword);
+    }
 
   }
 }
