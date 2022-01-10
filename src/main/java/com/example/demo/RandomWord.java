7782e887b9c3f4b39bf2405ac13ffacb33e57899
Ralph Bacolod1 <rafiks@gamma.devsphoto.com>
2022-01-09 22:54:37 -0500

    finally able to run compiled program

--- src/main/java/com/example/demo/RandomWord.java
+++ src/main/java/com/example/demo/RandomWord.java
@@ -1,8 +1,10 @@
-// package com.example.demo;
+package com.example.demo;
 
-import edu.princeton.cs.introcs.StdIn;
-import edu.princeton.cs.introcs.StdOut;
-import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.algs4.StdIn;
+
+// import edu.princeton.cs.introcs.StdIn;
+// import edu.princeton.cs.introcs.StdOut;
+// import edu.princeton.cs.introcs.StdRandom;
 
 public class RandomWord {
   public static void main(String[] args) {
@@ -12,8 +14,11 @@ public class RandomWord {
     // testing argument
     //
 
-    String test = StdIn.readString();
-    System.out.println("test");
+    String alll = StdIn.readString();
+    System.out.println(alll);
+    System.out.println("testing");
+    // String test = StdIn.readString();
+    // System.out.println("test");
     // StdIn.readString()
     // System.out.println("args-length: " + args.length); //
 
