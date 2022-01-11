27b08cc37e87e8da62edf8d0f955d14369e4215d
Ralph Bacolod1 <rafiks@gamma.devsphoto.com>
2022-01-10 21:07:25 -0500

    working knuth method with redirected file

--- src/main/java/com/example/demo/RandomWord.java
+++ src/main/java/com/example/demo/RandomWord.java
@@ -5,10 +5,18 @@ import edu.princeton.cs.algs4.StdRandom;
 
 public class RandomWord {
   public static void main(final String[] args) {
+    String champ = "";
+    double i = 1;
     while (!StdIn.isEmpty()) {
       String curword = StdIn.readString();
-      System.out.println(curword);
+      double p = 1.0 / i;
+      if (StdRandom.bernoulli(p)) {
+        champ = curword;
+      }
+      i++;
+
     }
 
+    System.out.println(champ);
   }
 }
